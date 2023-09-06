867d90067f9bdb5f7179b44595682997b9c298f9
KyleGong-alt <kylegong@ymail.com>
2023-09-05 22:58:37 -0700

    Better Memory Allocaiton

---
+++ Algorithms/JavaScript/234_PalindromeLinkedList.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let newArray = [];
+    while (true) { 
+        if (head && head.next) { // Check whether the current and next node exists
+            newArray.push(head.val);
+            head = head.next 
+        } else { // At the last element of LL
+            newArray.push(head.val); // Iterating through the LL
+            head = head.next // Update head to the next elements
+            break; // break out of the while loop since we're at the last element of LL
+        }
+    }
+
+    let reversed = [...newArray].reverse(); // Create a reversed shallow copy of array
+    for (let x = 0; x < newArray.length; x++) { // Iterate through array
+        if (reversed[x] !== newArray[x]) { // Check if reversed array matches original arr
+            return false;
+        }
+    }
+    return true;
+};
